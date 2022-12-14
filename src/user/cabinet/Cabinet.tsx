93a0b760bd33cd08a375879de8c171e938b8e488
Maksim Pegov <maksim.peg@gmail.com>
2022-12-14 13:04:55 +0100

    Cabinet styling

--- src/user/cabinet/Cabinet.tsx
+++ src/user/cabinet/Cabinet.tsx
@@ -1,8 +1,16 @@
+import { cn } from '@bem-react/classname'
+import { Avatar, InputBase, TextField } from '@mui/material'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { User } from 'shared/models/User'
 import { userActions } from 'user/state/user.reducer'
 import { selectUser } from 'user/state/user.selectors'
+import './Cabinet.scss'
+import Grid from '@mui/material/Unstable_Grid2'
+import { Edit } from '@mui/icons-material'
+
+const componentId = 'Cabinet'
+const bem = cn(componentId)
 
 export const Cabinet: React.FC = () => {
   const user = useSelector(selectUser) as User
@@ -28,19 +36,41 @@ export const Cabinet: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <p>
-        Your name:{' '}
-        {!changName ? (
-          user.name
-        ) : (
-          <input type={'changName'} defaultValue={user.name} onChange={changeName} />
-        )}{' '}
-        <button onClick={handleEditUsername}>edit</button>
-      </p>
+    <div className={bem()}>
+      <p className={bem('Title')}>User page</p>
+      <div className={bem('Body')}>
+        <Grid container spacing={2}>
+          <Grid className={bem('AvatarContainer')} xs={3}>
+            <Avatar className={bem('Avatar')}>{user.name.substring(0, 1)}</Avatar>
+          </Grid>
 
-      <button onClick={handleDelete}>DELETE ACCOUNT</button>
+          <Grid xs={9}>
+            <Grid container spacing={2}>
+              <Grid xs={12}>
+                <TextField
+                  className={bem('Input')}
+                  disabled
+                  id="outlined-disabled"
+                  label="User name"
+                  defaultValue={user.name}
+                />
+              </Grid>
+            </Grid>
+            <Grid xs={12}>
+              <TextField
+                className={bem('Input')}
+                disabled
+                id="outlined-disabled"
+                label="E-mail"
+                defaultValue={'user.emailsdfsdfsdfsdfsdfsd'}
+              />
+            </Grid>
+          </Grid>
+          {/* <Grid xs={12}>
+            <InputBase disabled />
+          </Grid> */}
+        </Grid>
+      </div>
     </div>
   )
 }
