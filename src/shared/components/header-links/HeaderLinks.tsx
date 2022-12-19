9ca57e15b6808c6c5952a4ecf9517eebd181e9c9
Maksim Pegov <maksim.peg@gmail.com>
2022-12-19 17:16:14 +0100

    App bar upgrade

---
+++ src/shared/components/header-links/HeaderLinks.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { cn } from '@bem-react/classname'
+import { Box, Link } from '@mui/material'
+import { motion } from 'framer-motion'
+import { ArrowDropDown } from '@mui/icons-material'
+
+import { SWAGGER } from 'app.constants'
+import './HeaderLinks.scss'
+
+const componentId = 'HeaderLinks'
+const bem = cn(componentId)
+
+export const HeaderLinks: React.FC<{ navigator: (location: string) => void }> = ({
+  navigator,
+}) => {
+  return (
+    <motion.div
+      className={bem()}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <Box className={bem('Links')}>
+        <form>Product</form>
+        <ArrowDropDown className={bem('Icon')} />
+      </Box>
+
+      <Box className={bem('Links')}>
+        <form>About Us</form>
+        <ArrowDropDown className={bem('Icon')} />
+      </Box>
+
+      <Box
+        className={bem('Links')}
+        onClick={() => {
+          window.location.replace(SWAGGER)
+        }}
+      >
+        <form>API</form>
+        <ArrowDropDown className={bem('Icon')} />
+      </Box>
+    </motion.div>
+  )
+}
--- src/shared/containers/app-header/AppHeader.tsx
+++ src/shared/containers/app-header/AppHeader.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react'
-import { Box, Button, Link } from '@mui/material'
+import { Box, Button, Link, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { motion } from 'framer-motion'
 import { cn } from '@bem-react/classname'
 import Grid from '@mui/material/Unstable_Grid2'
 
-import { UserControl } from 'shared/components/user-contol/UserControl'
 import { selectUser } from 'user/state/user.selectors'
-import { SWAGGER } from 'app.constants'
-import './AppHeader.scss'
+import { UserControl } from 'shared/components/user-contol/UserControl'
+import { HeaderLinks } from 'shared/components/header-links/HeaderLinks'
 import { UserNavigate } from 'shared/components/user-navigate/UserNavigate'
+import './AppHeader.scss'
 
 const componentId = 'AppHeader'
 const bem = cn(componentId)
@@ -20,75 +20,54 @@ export const AppHeader: React.FC = () => {
   const navigate = useNavigate()
 
   return (
-    <Grid container className={bem()} data-testid={bem()}>
-      <Grid xs={4}>
+    <Grid container spacing={2} className={bem()} data-testid={bem()}>
+      <Grid xs={3} display="flex">
         <Box className={bem('Logo')} onClick={() => navigate('/main')}>
-          <p className={bem('Audit')}>Audit</p>
-          <p className={bem('DB')}>DB</p>
+          <form className={bem('Audit')}>Audit</form>
+          <form className={bem('DB')}>DB</form>
         </Box>
       </Grid>
-      <Grid xs={4}>
-        {user === null ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Grid container>
-              <Grid xs={4}>
-                <Box>
-                  <p>Product</p>
-                </Box>
-              </Grid>
-              <Grid xs={4}>
-                <Box>
-                  <p>About Us</p>
-                </Box>
-              </Grid>
-              <Grid xs={4}>
-                <Box>
-                  <p>Community</p>
-                </Box>
-              </Grid>
-            </Grid>
-          </motion.div>
-        ) : null}
+      <Grid xs={0} md={5} display="flex">
+        {user === null ? <HeaderLinks navigator={navigate} /> : null}
       </Grid>
 
-      <Grid xs={4}>
+      <Grid xs={7} md={4} display="flex">
         {user === null ? (
           <motion.div
+            className={bem('Buttons')}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <Grid container spacing={2}>
-              <Grid xs={6}>
-                <Button
-                  variant="contained"
-                  color="warning"
-                  className={bem('Sign', { in: true })}
-                  data-testid={bem('SignIn')}
-                  onClick={() => navigate('/sign-in')}
-                >
-                  Sign In
-                </Button>
-              </Grid>
+            <Button
+              variant="contained"
+              color="warning"
+              className={bem('Sign', { in: true })}
+              data-testid={bem('SignIn')}
+              onClick={() => navigate('/sign-in')}
+            >
+              Sign In
+            </Button>
 
-              <Grid xs={6}>
-                <Button
-                  variant="outlined"
-                  className={bem('Sign', { up: true })}
-                  data-testid={bem('SignUp')}
-                  onClick={() => navigate('/sign-up')}
-                >
-                  Sign Up
-                </Button>
-              </Grid>
-            </Grid>
+            <Button
+              variant="outlined"
+              className={bem('Sign', { up: true })}
+              data-testid={bem('SignUp')}
+              onClick={() => navigate('/sign-up')}
+            >
+              Sign Up
+            </Button>
           </motion.div>
         ) : (
-          <UserControl user={user} />
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={bem('UserPanel')}
+          >
+            <UserNavigate navigator={navigate} />
+            <UserControl user={user} />
+          </motion.div>
         )}
       </Grid>
     </Grid>
