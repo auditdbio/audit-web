b4b6643e301c5b96b208c9539e7244d53176bda9
medetkazizov <m.kazizov@ejaw.net>
2023-02-12 17:53:50 +0600

    mark up card for customer

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from "../pages/HomePage.jsx";
 import SignupPage from "../pages/SignupPage.jsx";
 import SigninPage from "../pages/SigninPage.jsx";
+import HomeCustomer from "../pages/home-customer.jsx";
 
 
 const AppRoutes = () => {
@@ -12,6 +13,7 @@ const AppRoutes = () => {
                 <Route path={'/'} element={<HomePage />} />
                 <Route path={'sign-up'} element={<SignupPage />} />
                 <Route path={'sign-in'} element={<SigninPage />} />
+                <Route path={'home-customer'} element={<HomeCustomer />} />
             </Routes>
         </>
     );
