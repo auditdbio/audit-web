92d56aa58558a06b203f4d6e15ef24b96b676457
asd4a10 <rustem.amanzholov@nu.edu.kz>
2023-02-22 01:46:33 +0600

    almost done, adaptive for small screen and center

--- src/components/homepage/carousel/Carousel.jsx
+++ src/components/homepage/carousel/Carousel.jsx
@@ -1,13 +1,18 @@
+import "swiper/css/bundle";
+// import "swiper/css";
+// import Swiper from "swiper/bundle";
 import { Paper, Button, Box, Typography, Card } from "@mui/material";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { useRef } from "react";
 import { Navigation, Pagination } from "swiper";
-import "swiper/css/bundle";
-import "swiper/css";
 import FeedbackCard from "./FeedbackCard";
+import useMediaQuery from "@mui/material/useMediaQuery";
+// import theme from "../../../styles/themes";
+import { useEffect } from "react";
 
 const Carousel = () => {
 	const swiperRef = useRef(null);
+	const isMobile = useMediaQuery("(max-width: 768px)");
 
 	function handleNextClick() {
 		swiperRef.current.slideNext();
@@ -24,7 +29,13 @@ const Carousel = () => {
 		<Box
 			sx={{
 				paddingTop: "2rem",
-				maxWidth: "1480px",
+				width: {
+					xs: "450px",
+					sm: "700px",
+					md: "960px",
+					lg: "1480px",
+					xl: "1880px",
+				},
 			}}
 		>
 			<Box sx={carouselTextStyle}>Who’s using AuditDB?</Box>
@@ -34,36 +45,69 @@ const Carousel = () => {
 					justifyContent: "space-between",
 					alignItems: "center",
 					zIndex: 100,
+					margin: "0 auto",
+					gap: "10px",
 				}}
 			>
 				<Button
 					className={prevBtnClass}
 					onClick={handlePrevClick}
 					disableRipple
+					sx={{
+						display: {
+							xs: "none",
+							sm: "block",
+						},
+					}}
 				></Button>
 				<Swiper
-					className="my-slider"
-					modules={[Navigation, Pagination]}
-					spaceBetween={20}
-					slidesPerView={3}
+					slidesPerView={1}
 					onSlideChange={() => console.log("slide change")}
 					onSwiper={(swiper) => {
 						swiperRef.current = swiper;
 					}}
-					loop
-					centeredSlides={true}
-					style={swiperStyles.wrapper}
+					// pagination={{
+					// 	clickable: true,
+					// }}
+					modules={[Pagination, Navigation]}
+					breakpoints={{
+						920: {
+							slidesPerView: 2,
+							spaceBetween: 20,
+						},
+						1512: {
+							slidesPerView: 3,
+							spaceBetween: 50,
+						},
+						1920: {
+							slidesPerView: 4,
+							spaceBetween: 50,
+						},
+					}}
+					// loop={true}
+					// centeredSlides={true}
+					style={
+						{
+							// margin: "1rem",
+						}
+					}
 				>
 					{items.map((item) => (
-						<SwiperSlide key={item.id} style={swiperStyles.slide}>
+						<SwiperSlide key={item.id}>
 							<FeedbackCard info={item} />
 						</SwiperSlide>
 					))}
 				</Swiper>
 				<Button
 					className={nextBtnClass}
-					onClick={handleNextClick}
 					disableRipple
+					onClick={handleNextClick}
+					sx={{
+						display: {
+							xs: "none",
+							sm: "block",
+						},
+					}}
 				></Button>
 			</Box>
 		</Box>
@@ -79,9 +123,10 @@ const carouselTextStyle = {
 
 const swiperStyles = {
 	wrapper: {
-		paddingX: "2rem",
-		marginX: "2rem",
+		// paddingX: "2rem",
+		// marginX: "2rem",
 		boxShadow: "none",
+		margin: "0 auto",
 	},
 	slide: {
 		boxShadow: "none",
@@ -119,6 +164,27 @@ const items = [
 	},
 	{
 		id: 5,
+		name: "Artem Molodneckiy",
+		interests: "Criptography, Games",
+		description:
+			"“ As one of the first quadratic freelancers to go through the platform, my grant enabled me to leave my job and build open source tutorials and prototypes for the open internet. Super excited to see quadratic funding continue to help high leverage outliers find their place in our ecosystem.”",
+	},
+	{
+		id: 6,
+		name: "Artem Molodneckiy",
+		interests: "Criptography, Games",
+		description:
+			"“ As one of the first quadratic freelancers to go through the platform, my grant enabled me to leave my job and build open source tutorials and prototypes for the open internet. Super excited to see quadratic funding continue to help high leverage outliers find their place in our ecosystem.”",
+	},
+	{
+		id: 7,
+		name: "Artem Molodneckiy",
+		interests: "Criptography, Games",
+		description:
+			"“ As one of the first quadratic freelancers to go through the platform, my grant enabled me to leave my job and build open source tutorials and prototypes for the open internet. Super excited to see quadratic funding continue to help high leverage outliers find their place in our ecosystem.”",
+	},
+	{
+		id: 8,
 		name: "Artem Molodneckiy",
 		interests: "Criptography, Games",
 		description:
