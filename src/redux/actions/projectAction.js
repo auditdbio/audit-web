c2a3d89dd5795ddd6808049330057a027fa4fa5d
Rustem Amanzholov <52789182+asd4a10@users.noreply.github.com>
2023-03-10 16:23:30 +0600

    create request (#35)

--- src/redux/actions/projectAction.js
+++ src/redux/actions/projectAction.js
@@ -16,8 +16,9 @@ export const createProject = (values) => {
       tags: values.tags,
       publish: false,
       ready_to_wait: true,
-      prise_from: '0',
-      prise_to: '10000',
+      prise_from: "0",
+      prise_to: "10000",
+      creator_contacts: {},
     };
     const token = Cookies.get("token");
     axios
