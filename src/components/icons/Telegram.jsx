159a9c1002944578e70c0bbd290024003609addf
medetkazizov <m.kazizov@ejaw.net>
2023-03-14 19:56:53 +0600

    added link in icons in footer

--- src/components/icons/Telegram.jsx
+++ src/components/icons/Telegram.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Telegram = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
             <path
                 d="m20.665 3.717-17.73 6.837c-1.21.486-1.203 1.161-.222 1.462l4.552 1.42
                 10.532-6.645c.498-.303.953-.14.579.192l-8.533 7.701h-.002l.002.001-.314
