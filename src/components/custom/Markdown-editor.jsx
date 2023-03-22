3dd5f0b0ea387080915fec47c5f5464f9d04e4ff
medetkazizov <m.kazizov@ejaw.net>
2023-03-22 20:30:43 +0600

    added button for preview in markdown editor

--- src/components/custom/Markdown-editor.jsx
+++ src/components/custom/Markdown-editor.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Box, Typography} from "@mui/material";
+import {Box} from "@mui/material";
 import MarkdownEditor from "@uiw/react-markdown-editor";
 import {useField} from "formik";
 
