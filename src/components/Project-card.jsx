6d9930b75ec2ee23ac067231b89cc24d3a92b6c9
medetkazizov <m.kazizov@ejaw.net>
2023-04-25 14:53:59 +0600

    added status for project

--- src/components/Project-card.jsx
+++ src/components/Project-card.jsx
@@ -1,231 +1,248 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import {Box, Button, Typography} from "@mui/material";
 import Currency from "./icons/Currency.jsx";
 import Star from "./icons/Star.jsx";
-import theme, { radiusOfComponents } from "../styles/themes.js";
+import theme, {radiusOfComponents} from "../styles/themes.js";
 import {AUDITOR, DONE, SUBMITED} from "../redux/actions/types.js";
-import { useNavigate } from "react-router-dom/dist";
+import {useNavigate} from "react-router-dom/dist";
+import {useSelector} from "react-redux";
 
-const ProjectCard = ({ type, project }) => {
-  const navigate = useNavigate();
+const ProjectCard = ({type, project}) => {
+    const navigate = useNavigate();
+    const currentRole = useSelector(s => s.user.user.current_role)
 
-  const handleClick = () => {
-    if (type === AUDITOR) {
-      navigate(`/audit-request-offer/${project.id}`);
-    } else {
-      navigate(`/edit-project/${project.id}`);
-    }
-  };
+    const handleClick = () => {
+        if (type === AUDITOR) {
+            navigate(`/audit-request-offer/${project.id}`);
+        } else {
+            navigate(`/edit-project/${project.id}`);
+        }
+    };
 
-  const handleMakeCopy = () => {
-    navigate(`/edit-project/${project.id}?copy=true`);
-  };
+    const handleMakeCopy = () => {
+        navigate(`/edit-project/${project.id}?copy=true`);
+    };
 
-  return (
-    <Box sx={cardWrapper}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant={"h5"} textAlign={"center"}>
-          {project.name || project.project_name}
-        </Typography>
-        <Typography sx={categorySx}>{project?.tags?.map(el => el).join(', ') ?? ''}</Typography>
-        <Box sx={priceWrapper}>
-          <Box sx={infoWrapper}>
-            <Currency />
-            <Typography>{project.price}</Typography>
-          </Box>
-          <Box sx={infoWrapper}>
-            <Star />
-            <Typography>150</Typography>
-          </Box>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: 'auto',
-          marginBottom: 0
-        }}
-      >
-        <Box sx={statusWrapper}>
-          { project.status !== SUBMITED &&
-              <>
-                { project.status === DONE ?
-                    <Box sx={{backgroundColor: '#52176D'}} /> :
-                    project.status === 'pending' &&
-                    <Box sx={{backgroundColor: '#FF9900'}} />
+    return (
+        <Box sx={cardWrapper}>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography variant={"h5"} textAlign={"center"}>
+                    {project.name || project.project_name}
+                </Typography>
+                <Typography sx={categorySx}>{project?.tags?.map(el => el).join(', ') ?? ''}</Typography>
+                <Box sx={priceWrapper}>
+                    <Box sx={infoWrapper}>
+                        <Currency/>
+                        <Typography>{project.price}</Typography>
+                    </Box>
+                    <Box sx={infoWrapper}>
+                        <Star/>
+                        <Typography>150</Typography>
+                    </Box>
+                </Box>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    marginTop: 'auto',
+                    marginBottom: 0
+                }}
+            >
+                {currentRole === AUDITOR ?
+                    <Box sx={statusWrapper}>
+                        {project.status !== SUBMITED &&
+                            <>
+                                {project.status === DONE ?
+                                    <Box sx={{backgroundColor: '#52176D'}}/> :
+                                    project.status === 'pending' &&
+                                    <Box sx={{backgroundColor: '#FF9900'}}/>
+                                }
+                                {(project.status !== 'pending' && project.status !== DONE) &&
+                                    <Box sx={{backgroundColor: '#09C010'}}/>
+                                }
+                            </>
+                        }
+                        <Typography>
+                            {
+                                !project.status ? 'Waiting for audit' :
+                                    project.status === DONE ? 'Finished' : project.status === SUBMITED ? 'Submitted' : 'In progress'
+                            }
+                        </Typography>
+                    </Box>
+                    :
+                    <Box sx={statusWrapper}>
+                        {!project.publish_options.publish ?
+                                    <Box sx={{backgroundColor: '#FF9900'}}/>
+                                :
+                                    <Box sx={{backgroundColor: '#09C010'}}/>
+                        }
+                        <Typography>
+                            {
+                                project.publish_options.publish ? 'Publish' : 'Hidden'
+                            }
+                        </Typography>
+                    </Box>
                 }
-                { (project.status !== 'pending' && project.status !== DONE) &&
-                    <Box sx={{backgroundColor: '#09C010'}} />
-                }
-              </>
-          }
-          <Typography>
-            {
-              !project.status ? 'Waiting for audit' :
-                  project.status === DONE ? 'Finished' : project.status === SUBMITED ? 'Submitted' : 'In progress'
-            }
-          </Typography>
+                <Button
+                    variant={"contained"}
+                    sx={[editButton, type === "auditor" ? editAuditor : {}]}
+                    onClick={handleClick}
+                >
+                    {type === AUDITOR ? "Submit" : "Edit"}
+                </Button>
+                {project.name && <Button sx={copyBtn} onClick={handleMakeCopy}>Make a copy</Button>}
+            </Box>
         </Box>
-        <Button
-          variant={"contained"}
-          sx={[editButton, type === "auditor" ? editAuditor : {}]}
-          onClick={handleClick}
-        >
-          {type === AUDITOR ? "Submit" : "Edit"}
-        </Button>
-        {project.name && <Button sx={copyBtn} onClick={handleMakeCopy}>Make a copy</Button>}
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default ProjectCard;
 
 const priceWrapper = (theme) => ({
-  display: "flex",
-  gap: "30px",
-  mt: "18px",
-  [theme.breakpoints.down("md")]: {
-    gap: "18px",
-  },
-  [theme.breakpoints.down("xs")]: {
-    mt: "5px",
-  },
+    display: "flex",
+    gap: "30px",
+    mt: "18px",
+    [theme.breakpoints.down("md")]: {
+        gap: "18px",
+    },
+    [theme.breakpoints.down("xs")]: {
+        mt: "5px",
+    },
 });
 
 const copyBtn = (theme) => ({
-  textTransform: "none",
-  fontSize: "10px",
-  mt: "12px",
-  [theme.breakpoints.down("xs")]: {
-    mt: "5px",
-  },
+    textTransform: "none",
+    fontSize: "10px",
+    mt: "12px",
+    [theme.breakpoints.down("xs")]: {
+        mt: "5px",
+    },
 });
 
 const statusWrapper = (theme) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  gap: "15px",
-  width: "100%",
-  "& p": {
-    fontSize: "10px",
-    fontWeight: 500,
-    color: "#434242",
-  },
-  "& div": {
-    width: "17px",
-    height: "17px",
-    borderRadius: "50%",
-  },
-  margin: "40px 0 18px",
-  [theme.breakpoints.down("md")]: {
-    margin: "25px 0 10px",
-  },
-  [theme.breakpoints.down("xs")]: {
-    marginTop: 0,
-    gap: "10px",
-    "& div": {
-      width: "8px",
-      height: "8px",
-      borderRadius: "50%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "15px",
+    width: "100%",
+    "& p": {
+        fontSize: "10px",
+        fontWeight: 500,
+        color: "#434242",
     },
-  },
+    "& div": {
+        width: "17px",
+        height: "17px",
+        borderRadius: "50%",
+    },
+    margin: "40px 0 18px",
+    [theme.breakpoints.down("md")]: {
+        margin: "25px 0 10px",
+    },
+    [theme.breakpoints.down("xs")]: {
+        marginTop: 0,
+        gap: "10px",
+        "& div": {
+            width: "8px",
+            height: "8px",
+            borderRadius: "50%",
+        },
+    },
 });
 
 const editButton = (theme) => ({
-  fontSize: "15px",
-  fontWeight: 600,
-  lineHeight: "25px",
-  width: "110px",
-  textTransform: "none",
-  borderRadius: "10px",
-  gap: "40px",
-  padding: "9px 0",
-  maxWidth: "170px",
-  [theme.breakpoints.down("md")]: {
-    width: "100px",
-    height: "30px",
-  },
-  [theme.breakpoints.down("xs")]: {
-    fontSize: "11px",
-  },
+    fontSize: "15px",
+    fontWeight: 600,
+    lineHeight: "25px",
+    width: "110px",
+    textTransform: "none",
+    borderRadius: "10px",
+    gap: "40px",
+    padding: "9px 0",
+    maxWidth: "170px",
+    [theme.breakpoints.down("md")]: {
+        width: "100px",
+        height: "30px",
+    },
+    [theme.breakpoints.down("xs")]: {
+        fontSize: "11px",
+    },
 });
 
 const editAuditor = (theme) => ({
-  backgroundColor: theme.palette.secondary.main,
-  "&:hover": {
-    backgroundColor: "#450e5d",
-  },
+    backgroundColor: theme.palette.secondary.main,
+    "&:hover": {
+        backgroundColor: "#450e5d",
+    },
 });
 
 const infoWrapper = (theme) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  "& p": {
-    fontSize: "12px!important",
-    marginLeft: "8px",
-  },
-  [theme.breakpoints.down("xs")]: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
     "& p": {
-      fontSize: "10px!important",
+        fontSize: "12px!important",
+        marginLeft: "8px",
     },
-    "& svg": {
-      width: "10px",
-      height: "10px",
+    [theme.breakpoints.down("xs")]: {
+        "& p": {
+            fontSize: "10px!important",
+        },
+        "& svg": {
+            width: "10px",
+            height: "10px",
+        },
     },
-  },
 });
 
 const categorySx = (theme) => ({
-  textAlign: "center",
-  fontSize: "12px!important",
-  fontWeight: 500,
-  color: "#434242",
-  margin: "10px 0 7px",
-  [theme.breakpoints.down("xs")]: {
-    fontSize: "10px!important",
-  },
+    textAlign: "center",
+    fontSize: "12px!important",
+    fontWeight: 500,
+    color: "#434242",
+    margin: "10px 0 7px",
+    [theme.breakpoints.down("xs")]: {
+        fontSize: "10px!important",
+    },
 });
 
 const cardWrapper = (theme) => ({
-  display: "flex",
-  flexDirection: "column",
-  padding: "31px 48px 37px",
-  height: '100%',
-  boxShadow:
-    "0px 64.1377px 76.5824px rgba(0, 0, 0, 0.07)," +
-    " 0px 14.326px 17.1057px rgba(0, 0, 0, 0.0417275)," +
-    "0px 8.03104px 9.5893px rgba(0, 0, 0, 0.035), " +
-    "0px 4.26523px 5.09281px rgba(0, 0, 0, 0.0282725), " +
-    "0px 1.77486px 2.11923px rgba(0, 0, 0, 0.0196802)",
-  borderRadius: "25px",
-  border: "1px solid rgba(67, 66, 66, 0.1)",
-  alignItems: "center",
-  "& h5": {
-    fontSize: "18px",
-    fontWeight: 500,
-    lineHeight: "22px",
-  },
-  [theme.breakpoints.down("md")]: {
-    padding: "33px 22px 24px",
-  },
-  [theme.breakpoints.down("xs")]: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    padding: "15px 10px",
+    display: "flex",
+    flexDirection: "column",
+    padding: "31px 48px 37px",
+    height: '100%',
+    boxShadow:
+        "0px 64.1377px 76.5824px rgba(0, 0, 0, 0.07)," +
+        " 0px 14.326px 17.1057px rgba(0, 0, 0, 0.0417275)," +
+        "0px 8.03104px 9.5893px rgba(0, 0, 0, 0.035), " +
+        "0px 4.26523px 5.09281px rgba(0, 0, 0, 0.0282725), " +
+        "0px 1.77486px 2.11923px rgba(0, 0, 0, 0.0196802)",
+    borderRadius: "25px",
+    border: "1px solid rgba(67, 66, 66, 0.1)",
+    alignItems: "center",
     "& h5": {
-      fontSize: "14px",
-      fontWeight: 500,
+        fontSize: "18px",
+        fontWeight: 500,
+        lineHeight: "22px",
     },
-  },
+    [theme.breakpoints.down("md")]: {
+        padding: "33px 22px 24px",
+    },
+    [theme.breakpoints.down("xs")]: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: "15px 10px",
+        "& h5": {
+            fontSize: "14px",
+            fontWeight: 500,
+        },
+    },
 });
