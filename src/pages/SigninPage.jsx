c3e1cdfa99c5aefc562ea7c9f433d5533c63d666
medetkazizov <m.kazizov@ejaw.net>
2023-06-02 14:37:21 +0600

    redirect user from sign up and sign in if authenticated

--- src/pages/SigninPage.jsx
+++ src/pages/SigninPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
 import { ContentWrapper } from '../styles/themes.js';
 import Layout from '../styles/Layout.jsx';
 import { CustomCard } from '../components/custom/Card';
 import SigninForm from '../components/forms/signin/SigninForm.jsx';
+import { useNavigate } from 'react-router-dom';
+import { isAuth } from '../lib/helper.js';
 
 const SigninPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuth()) {
+      navigate('/');
+    }
+  }, [isAuth()]);
   return (
     <Layout>
       <CustomCard sx={cardWrapper}>
