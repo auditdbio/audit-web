f30becdf8e93b217d8e0d506ac5afee359b697b1
medetkazizov <m.kazizov@ejaw.net>
2023-06-05 16:54:32 +0600

    Revert "fixes of request if user didnt authenticated"
    This reverts commit 0d027e910b6698ea012e6fd2ec6d6233c063d0ab.

--- src/redux/actions/auditorAction.js
+++ src/redux/actions/auditorAction.js
@@ -35,14 +35,9 @@ export const getCurrentAuditor = id => {
   const token = Cookies.get('token');
   return dispatch => {
     axios
-      .get(
-        `${API_URL}/auditor/${id}`,
-        token
-          ? {
-              headers: { Authorization: `Bearer ${token}` },
-            }
-          : {},
-      )
+      .get(`${API_URL}/auditor/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
       .then(({ data }) => {
         dispatch({ type: GET_CURRENT_AUDITOR, payload: data });
       })
