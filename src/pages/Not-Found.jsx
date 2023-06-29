29e88f245625714ad707358561282f19432a2fbb
medetkazizov <m.kazizov@ejaw.net>
2023-06-29 15:39:02 +0600

    added new 404 design

--- src/pages/Not-Found.jsx
+++ src/pages/Not-Found.jsx
@@ -7,6 +7,7 @@ import { addTestsLabel } from '../lib/helper.js';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from 'react-router-dom/dist';
 import { AUDITOR } from '../redux/actions/types.js';
+import NotFound404 from '../components/images/404.png';
 
 const NotFound = ({ role }) => {
   const navigate = useNavigate();
@@ -23,13 +24,7 @@ const NotFound = ({ role }) => {
           <ArrowBackIcon />
         </Button>
         <Box>
-          <Typography variant={'h3'}>Page not found</Typography>
-          <Typography>
-            Oops! The page you are looking for does not exist
-          </Typography>
-          <Typography variant={'h2'} color={'primary'}>
-            404
-          </Typography>
+          <img src={NotFound404} alt="" />
         </Box>
       </CustomCard>
     </Layout>
@@ -47,63 +42,19 @@ const backButtonSx = theme => ({
   },
   [theme.breakpoints.down('sm')]: {
     top: 0,
+    left: 0,
   },
 });
 
 const wrapper = theme => ({
-  padding: '48px 74px 80px',
+  padding: '25px',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   gap: '80px',
   position: 'relative',
-  '& div': {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: '15px',
-    marginTop: '50px',
-    '& h3': {
-      fontSize: '24px',
-      lineHeight: '29px',
-      fontWeight: 'bold',
-      color: theme.palette.text.primary,
-    },
-    '& h2': {
-      fontSize: '144px',
-      lineHeight: '100%',
-      fontWeight: 'bold',
-    },
-  },
-  [theme.breakpoints.down('md')]: {
-    padding: '38px 44px 60px',
-  },
-  [theme.breakpoints.down('sm')]: {
-    minHeight: '70vh',
-    padding: '38px 20px 30px',
-    '& div': {
-      marginTop: '20px',
-      '& h3': {
-        fontSize: '20px',
-      },
-      '& h2': {
-        fontSize: '124px',
-      },
-    },
-  },
-  [theme.breakpoints.down('xs')]: {
-    '& div': {
-      '& p': {
-        fontSize: '12px',
-        textAlign: 'center',
-      },
-      marginTop: '20px',
-      '& h3': {
-        fontSize: '20px',
-      },
-      '& h2': {
-        fontSize: '94px',
-      },
-    },
+  '& img': {
+    width: '100%',
+    objectFit: 'contain',
   },
 });
