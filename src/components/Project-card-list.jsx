deeee5f2ba95115700854d6168d95571eeaba149
medetkazizov <m.kazizov@ejaw.net>
2023-06-29 19:18:28 +0600

    made project list component

---
+++ src/components/Project-card-list.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import ProjectCard from './Project-card.jsx';
+
+const ProjectCardList = ({ projects, role }) => {
+  return (
+    <Grid container spacing={2}>
+      {projects?.map(project => (
+        <Grid key={project.id} item sx={gridItemStyle}>
+          <ProjectCard project={project} type={role} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default ProjectCardList;
+
+const gridItemStyle = theme => ({
+  width: '25%',
+  [theme.breakpoints.down('sm')]: {
+    width: '33.330%',
+  },
+  [theme.breakpoints.down('xs')]: {
+    width: '100%',
+  },
+});
