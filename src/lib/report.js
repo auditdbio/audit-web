744f2ec41d01d55ddd1bc56225737a0ca2339f14
samartsevigor <i.samartsev@godeltech.com>
2023-07-24 17:12:38 +0600

    fixed generate & download report

--- src/lib/report.js
+++ src/lib/report.js
@@ -3,12 +3,17 @@ import { API_URL, ASSET_URL } from '../services/urls.js';
 import Cookies from 'js-cookie';
 
 const downloadResponse = (res, audit) => {
+  console.log(res.data);
   const url = window.URL.createObjectURL(new Blob([res.data]));
   const link = document.createElement('a');
   link.href = url;
   link.setAttribute(
     'download',
-    `${audit?.report_name ? audit?.report_name : audit?.project_name + '.pdf'}`,
+    `${
+      audit?.report_name
+        ? audit?.report_name
+        : audit?.project_name + ' report.pdf'
+    }`,
   );
   document.body.appendChild(link);
   link.click();
@@ -28,9 +33,14 @@ export const generateReport = audit => {
   const token = Cookies.get('token');
   axios
     .post(`${API_URL}/report/${audit.id}`, null, {
-      responseType: 'blob',
-      withCredentials: true,
       headers: { Authorization: `Bearer ${token}` },
     })
+    .then(({ data }) =>
+      axios.get(`${ASSET_URL}/${data.path}`, {
+        responseType: 'blob',
+        withCredentials: true,
+        headers: { Authorization: `Bearer ${Cookies.get('token')}` },
+      }),
+    )
     .then(response => downloadResponse(response, audit));
 };
