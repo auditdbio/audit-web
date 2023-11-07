aebd4250e05da1711b201fc53468369e789d967a
medetkazizov <m.kazizov@ejaw.net>
2023-11-07 16:18:52 +0600

    fixes of tags field

--- src/components/forms/tags-field/tags-field.jsx
+++ src/components/forms/tags-field/tags-field.jsx
@@ -44,7 +44,8 @@ const TagsField = ({
           setState('');
           if (handleSubmit) handleSubmit();
         } else {
-          setError('Invalid link');
+          fieldHelper.setValue([...field.value, `https://${state}`]);
+          setState('');
         }
       } else {
         setError('The maximum number of links that can be added is 20');
