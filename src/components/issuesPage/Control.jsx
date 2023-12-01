45e594f7dbbefb634e2e0f7151e76b2523f722e0
medetkazizov <m.kazizov@ejaw.net>
2023-12-01 20:21:50 +0600

    added disclaimer in audit report

--- src/components/issuesPage/Control.jsx
+++ src/components/issuesPage/Control.jsx
@@ -84,6 +84,21 @@ const Control = ({
           project_name: report.project_name,
           report_data: [
             {
+              type: 'markdown',
+              title: 'Disclaimer',
+              text:
+                '\n' +
+                '## Important to remember:\n' +
+                '\n' +
+                "1. This audit was performed based on the current state of the code at the time of evaluation. Any subsequent changes or modifications to the codebase could render auditors' findings obsolete. Re-audit is recommended post any alterations.\n" +
+                '\n' +
+                '2. While we strive for accuracy, auditors cannot guarantee that all potential vulnerabilities or bugs have been identified. The auditor is not responsible for any overlooked issues.\n' +
+                '\n' +
+                "3. It's always recommended to have multiple layers of checks and balances, including but not limited to, regular code reviews and updated audits.",
+              include_in_toc: true,
+              subsections: [],
+            },
+            {
               type: 'plain_text',
               title: 'Summary',
               include_in_toc: true,
