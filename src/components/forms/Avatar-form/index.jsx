d317dc97e24a18a0f8551c1519d62a9fe6bb5f00
samartsevigor <i.samartsev@godeltech.com>
2023-12-01 19:30:43 +0600

    fixed user avatar

--- src/components/forms/Avatar-form/index.jsx
+++ src/components/forms/Avatar-form/index.jsx
@@ -5,6 +5,7 @@ import Cookies from 'js-cookie';
 import { useSelector } from 'react-redux';
 import { Avatar, Button } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit.js';
+import ClearIcon from '@mui/icons-material/Clear';
 import { AUDITOR } from '../../../redux/actions/types.js';
 import theme from '../../../styles/themes.js';
 import { ASSET_URL } from '../../../services/urls.js';
@@ -12,7 +13,7 @@ import CustomSnackbar from '../../custom/CustomSnackbar.jsx';
 import { addTestsLabel } from '../../../lib/helper.js';
 
 const AvatarForm = ({ role, name }) => {
-  const user = useSelector(state => state.user.user);
+  const { user } = useSelector(state => state.user);
   const [avatarField, , fieldHelper] = useField(name);
   const formData = new FormData();
   const [error, setError] = useState(null);
@@ -42,41 +43,52 @@ const AvatarForm = ({ role, name }) => {
 
   const handleUpdateAvatar = e => {
     const file = e.target.files[0];
-    const fileSize = file.size;
-    if (fileSize > 10000000) {
-      return setError('File size is too big');
-    } else {
-      formData.append('file', file);
-      formData.append('path', user.id + user.current_role + file.name);
-      formData.append('original_name', file.name);
-      formData.append('private', 'false');
-      sendAvatar();
+    if (file) {
+      const fileSize = file.size;
+      if (fileSize > 10000000) {
+        return setError('File size is too big');
+      } else {
+        formData.append('file', file);
+        formData.append('path', user.id + user.current_role + file.name);
+        formData.append('original_name', file.name);
+        formData.append('private', 'false');
+        sendAvatar();
+      }
     }
   };
 
   useEffect(() => {
-    const isThirdPartyImage = /^https?:\/\//i.test(avatarField.value);
-    if (isThirdPartyImage) {
-      axios
-        .get(avatarField.value, { responseType: 'blob' })
-        .then(({ data }) => {
-          const filename =
-            user.id +
-            user.current_role +
-            Date.now() +
-            data.type.replace(/image\//, '.');
-          formData.append('file', data);
-          formData.append('path', filename);
-          formData.append('original_name', filename);
-          formData.append('private', 'false');
-          sendAvatar();
-        });
+    if (user.is_new && !avatarField.value) {
+      const avatar = user?.linked_accounts?.[0]?.avatar;
+      const isThirdPartyImage = /^https?:\/\//i.test(avatar);
+
+      if (isThirdPartyImage) {
+        axios
+          .get(avatarField.value, { responseType: 'blob' })
+          .then(({ data }) => {
+            const filename =
+              user.id +
+              user.current_role +
+              Date.now() +
+              data.type.replace(/image\//, '.');
+            formData.append('file', data);
+            formData.append('path', filename);
+            formData.append('original_name', filename);
+            formData.append('private', 'false');
+            sendAvatar();
+          });
+      }
     }
-  }, []);
+  }, [user]);
+
+  const deletePhoto = () => {
+    fieldHelper.setValue('');
+  };
 
   return (
     <>
       <Avatar
+        sx={{ mb: '35px' }}
         src={
           avatarField.value &&
           !/^https?:\/\//i.test(avatarField.value) &&
@@ -98,16 +110,27 @@ const AvatarForm = ({ role, name }) => {
       >
         <label
           htmlFor="file-upload"
-          style={{ display: 'flex', alignItems: 'center' }}
+          style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
         >
-          <EditIcon fontSize={'small'} />
+          <EditIcon fontSize="small" />
           Edit photo
         </label>
       </Button>
+      {avatarField.value && (
+        <Button
+          sx={deletePhotoSx(role)}
+          onClick={deletePhoto}
+          {...addTestsLabel('delete-photo-button')}
+        >
+          <ClearIcon fontSize="small" />
+          Delete photo
+        </Button>
+      )}
+
       <input
-        id={'file-upload'}
+        id="file-upload"
         style={{ display: 'none' }}
-        accept={'.jpg,.png,.jpeg,.gif,.bmp,.tif,.tiff,.webp'}
+        accept=".jpg,.png,.jpeg,.gif,.bmp,.tif,.tiff,.webp"
         onChange={handleUpdateAvatar}
         type="file"
       />
@@ -116,3 +139,12 @@ const AvatarForm = ({ role, name }) => {
 };
 
 export default AvatarForm;
+
+const deletePhotoSx = role => ({
+  display: 'flex',
+  alignItems: 'center',
+  color:
+    role === AUDITOR
+      ? theme.palette.secondary.main
+      : theme.palette.primary.main,
+});
