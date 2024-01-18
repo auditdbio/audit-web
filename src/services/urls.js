a8143c50c3483e1843554f087982468390440216
medetkazizov <m.kazizov@ejaw.net>
2024-01-18 19:19:33 +0600

    changed url twiiter_client_id to VITE_TWITTER_CLIENT_ID

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,6 +1,6 @@
 export const API_URL = import.meta.env.VITE_API_BASE_URL;
 export const BASE_URL = import.meta.env.BASE_URL;
-export const TWITTER_CLIENT_ID = import.meta.env.TWITTER_CLIENT_ID;
+export const TWITTER_CLIENT_ID = import.meta.env.VITE_TWITTER_CLIENT_ID;
 export const GITHUB_CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENT_ID;
 export const LINKEDIN_CLIENT_ID = import.meta.env.VITE_LINKEDIN_CLIENT_ID;
 export const ASSET_URL = `${API_URL}/file`;
