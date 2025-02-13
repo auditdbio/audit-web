a27d48f5a68e6c471331e663ac0d74b4d9f99a98
medetkazizov <m.kazizov@ejaw.net>
2025-02-13 16:50:05 +0600

    changed max price

--- src/components/forms/TotalPrice/TotalPrice.jsx
+++ src/components/forms/TotalPrice/TotalPrice.jsx
@@ -34,7 +34,7 @@ const TotalPrice = () => {
         Total cost
       </Box>
       <SalarySlider
-        max={isTotalCost ? 500000 : 1000}
+        max={isTotalCost ? 500000 : 100}
         name={isTotalCost ? 'total_cost' : 'price'}
       />
     </>
